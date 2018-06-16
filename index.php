c44581c451217019b12f068185db33f1c53f80e7
Beron99 <j_oliveiras12@hotmail.com>
2018-06-16 12:05:29 -0300

    template admin adicionado

--- index.php
+++ index.php
@@ -4,19 +4,31 @@ require_once("vendor/autoload.php");//Tras as dependencias do que o projeto prec
 
 use \Slim\Slim;//classes carregadas
 use \Hcode\Page;//classes carregadas
+use \Hcode\PageAdmin;//classes carregadas
+
+
 
 $app = new Slim();//slim gerencias as rotas da url
 
 $app->config('debug', true);
 
+//Roda do index
 $app->get('/', function() {//quando chamarem via get a pasta raiz o que esta na rota
 
 	$page = new Page();
 
 	$page->setTpl("index");//carrega o header, body, footer
-    
-    
-	
+   
+
+});
+
+//Roda da page admin
+$app->get('/admin/', function() {//quando chamarem via get a pasta raiz o que esta na rota
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");//carrega o header, body, footer
+   
 
 });
 
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,20 @@
+<?php 
+
+	namespace Hcode;
+
+	class PageAdmin extends Page {
+
+		public function __construct($opts = array(), $tpl_dir = "/views/admin/")//local do arquivos html pagina admin
+
+			
+		{
+
+			parent::__construct($opts, $tpl_dir);// acessar metodo da classe pai
+
+		} 
+
+	}	
+
+ ?>
+
+
