3f7707433efaf8ae1ce94c1fe35fda8b7ea8af34
Beron99 <j_oliveiras12@hotmail.com>
2018-06-17 13:28:01 -0300

    erro na classe User.php(101, Model.php(17)

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $app->get('/', function() {//quando chamarem via get a pasta raiz o que esta na 
 
 //Roda da page admin
 $app->get('/admin', function() {//quando chamarem via get a pasta raiz o que esta na rota
-	User::verifyLogin();	
+	User::verifyLogin();//serifica se a pessoa está logada na sessao	
 
 	$page = new PageAdmin();
 
@@ -69,6 +69,78 @@ $app->get('/admin/logout', function() {//recebe o post da pagina login
 	exit;
 
 });
+
+$app->get('/admin/users', function() {//tela que lista todos os usurios
+
+	User::verifyLogin();//serifica se a pessoa está logada no sistema	
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+
+		"users"=>$users
+	));	
+		
+});
+
+$app->get('/admin/users/create', function() {//tela que lista todos os usurios
+
+	User::verifyLogin();//serifica se a pessoa está logada no sistema	
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");	
+		
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser) {//roda de delete pagina com id usuario
+
+	User::verifyLogin();// verifica se usuario está logado no sistema
+	
+});
+
+$app->get('/admin/users/:iduser', function($iduser){
+ 
+   User::verifyLogin();
+ 
+   $user = new User();
+ 
+   $user->get((int)$iduser);
+ 
+   $page = new PageAdmin();
+ 
+   $page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
+ 
+});
+
+$app->post("/admin/users/create", function() {
+
+	User::verifyLogin();//verifica se o usuario está logado
+
+	$user = new User();
+
+	$_POST["iadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->post("/admin/users/:iduser", function($iduser) {//roda d uptade por id de usuario
+
+	User::verifyLogin();
+	
+});
+
+
+
 
 
 $app->run();//depois que carregar todos os templates, executa
